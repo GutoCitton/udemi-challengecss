0baf793637ddc82d040f9d0016c51bbd111e546a
Augusto Citton Ludwig <augusto.citton@dbccompany.com.br>
2022-11-21 12:06:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import CarDetail from "./components/carDetail";
 import './App.css';
+
+const cars = [
+  {id: 1, brand: 'Ferrari', color: 'Amarelo', newCar: true, km: 0},
+  {id: 2, brand: 'Kia', color: 'Branco', newCar: false, km: 2341},
+  {id: 3, brand: 'Renault', color: 'Azul', newCar: false, km: 67899},
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="div1">
+        <h1 className="title_test">Teste</h1>
+      </div>
+
+      {cars.map((car)=> (
+        <CarDetail
+        key={car.id}
+        brand={car.brand}
+        color={car.color}
+        km= {car.km}
+        newCar={car.newCar}
+        />
+      ))}
     </div>
   );
 }
